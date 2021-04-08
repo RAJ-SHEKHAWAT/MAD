1c3792cd92d7706572371b907017386aef52ff15
Pranav Singhal <iit2019050@iiita.ac.in>
2021-04-08 20:51:43 +0530

    Added

--- lib/api/firebase_ml_api.dart
+++ lib/api/firebase_ml_api.dart
@@ -1,258 +1,259 @@
-//import 'dart:io';
-//
-//import 'package:firebase_ml_vision/firebase_ml_vision.dart';
-//
-//class FirebaseMLApi {
-//
-//
-//  int medicineNameBlock;
-//  String medicineName2 = "";
-//  String medicineName1 = "";
-//  int directionsOfUseBlock = 100000;
-//  String unitOfMeasure = null;
-//  String numberOfUnits = null;
-//  String dayFrequency = null;
-//  String frequency = null;
-//  List<dynamic> blocks =[];
-//  static Future<String> recogniseText(File imageFile) async {
-//    if (imageFile == null) {
-//      return 'No selected image';
-//    } else {
-//      dynamic visionImage = FirebaseVisionImage.fromFile(imageFile);
-//     dynamic textRecognizer = FirebaseVision.instance.textRecognizer();
-//      try {
-//        dynamic visionText = await textRecognizer.processImage(visionImage);
-//        await textRecognizer.close();
-//
-//        final text = extractText(visionText);
-//        return text.isEmpty ? 'No text found in the image' : text;
-//      } catch (error) {
-//        return error.toString();
-//      }
-//    }
-//  }
-//
-//   String extractText(VisionText visionText) {
+import 'dart:io';
+
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+
+class FirebaseMLApi {
+
+
+  int medicineNameBlock;
+  String medicineName2 = "";
+  String medicineName1 = "";
+  int directionsOfUseBlock = 100000;
+  String unitOfMeasure = null;
+  String numberOfUnits = null;
+  String dayFrequency = null;
+  String frequency = null;
+  List<dynamic> blocks =[];
+  static Future<String> recogniseText(File imageFile) async {
+    if (imageFile == null) {
+      return 'No selected image';
+    } else {
+      dynamic visionImage = FirebaseVisionImage.fromFile(imageFile);
+     dynamic textRecognizer = FirebaseVision.instance.textRecognizer();
+      try {
+        dynamic visionText = await textRecognizer.processImage(visionImage);
+        await textRecognizer.close();
+
+        final text = extractText(visionText);
+        return text.isEmpty ? 'No text found in the image' : text;
+      } catch (error) {
+        return error.toString();
+      }
+    }
+  }
+
+   static extractText(VisionText visionText) {
 //    categorizeBlocks(visionText);
 //    String text = "Medicine Name: " + medicineName1 + " " + medicineName2 + "\n" + "Take " + numberOfUnits + " " + unitOfMeasure + " " + frequency + " times " + dayFrequency;
 ////    text+="Medcine Name:";
 ////    text+=medicine
 ////
-////    for (TextBlock block in visionText.blocks) {
-////      for (TextLine line in block.lines) {
-////        for (TextElement word in line.elements) {
-////          text = text + word.text + ' ';
-////        }
-////        text = text + '\n';
-////      }
-////    }
-//
-//    return text;
-//  }
-//
-//  void categorizeBlocks(VisionText visionText) {
-//
-//    blocks = visionText.blocks;
-//    if (blocks.isEmpty) {
-////    Toast.makeText(this, "No text in image", Toast.LENGTH_SHORT).show();
-//      print('No text');
-//    }
-//    else {
-//      int i=0;
-//      for (TextBlock block in visionText.blocks) {
-//        // System.out.println(blocks.get(i).getText().toString().toLowerCase());
-//        List<TextLine> paras = block.lines;
-//        for (TextLine para in paras) {
-//          List<TextElement> elements = para.elements;
-//          //System.out.println(elements.get(2).getText());
-//          for (int k=0;k<elements.length;k++) {
-//            String word = elements[k].toString().toLowerCase();
-//
-//            if (((word == "take") || (word == "taken")) &&
-//                (directionsOfUseBlock == 100000)) {
-//              print("FOUND DIRECTIONS");
-//              directionsOfUseBlock = i;
-////              extractDetails(blocks);
-//            }
-//            String mySubstring = null;
-//            if (word.length > 2) {
-//              mySubstring = word.substring(word.length - 2, word.length);
-//            }
-//
-//            //System.out.println(mySubstring);
-//            if (word.length > 2 && ((mySubstring.compareTo("mg")) == 0 ||
-//                (mySubstring.compareTo("ml")) == 0)) {
-//              print("you should only see this once");
-//              //System.out.println(word);
-//              medicineNameBlock = i;
-//              if (k - 1 > -1) {
-//                //System.out.println(elements.get(k - 1));
-//                medicineName2 =  elements[k - 1].toString();
-//
-//                if (k - 2 > -1) {
-//                  // System.out.println("med1");
-//                  medicineName1 = elements[k - 2].toString();
-//                }
-//              }
-//            }//else if (word.length() > 1 && (word.substring(word.length() - 1, word.length() - 1).equals("g") || (word.substring(word.length() - 1, word.length() - 1).equals("l")))) {
-//            //medicineNameBlock = i;
-//          } //System.out.println("Next statement");
-//        }
-//        i++;
-//      }
-//    }
-//  }
-//
-//  void extractDetails(List<TextBlock> blocks) {
-//    List<TextLine> paras = blocks[directionsOfUseBlock].lines;
-//    for (int j = 0; j < paras.length; j++) {
-//      List<TextElement> words = paras[j].elements;
-//
-//      for (int k = 0; k < words.length; k++) {
-//        String word = words[k].toString().toLowerCase().trim();
-//        //System.out.println(word);
-//        String prevWord = null;
-//        if (k != 0) {
-//          prevWord =words[k-1].toString().toLowerCase().trim();
-//        }
-//        switch (word) {
-//          case "daily":
-//          case "weekly":
-//          case "monthly":
-//            dayFrequency = word;
-//            break;
-//          case "day":
-//            if (prevWord=="a") {
-//              dayFrequency = "daily";
-//            }
-//            break;
-//          case "week":
-//            if (prevWord=="a") {
-//              dayFrequency = "weekly";
-//            }
-//            break;
-//          case "month":
-//            if (prevWord=="a") {
-//              dayFrequency = "monthly";
-//            }
-//            break;
-//          case "once":
-//          case "time":
-//            frequency = "1";
-//            break;
-//          case "twice":
-//            frequency = "2";
-//            break;
-//          case "thrice":
-//            frequency = "3";
-//            break;
-//          case "times":
-//            if (checkIfWordNumber(prevWord)) { //checks if its a number spelt out as a word
-//              frequency = convertWordNumber(prevWord); //converts the word into a number but in string format
-//            } else if (checkIfNumber(prevWord)) {
-//              frequency = prevWord;
-//            }
-//            break;
-//          case "tablet":
-//          case "capsule":
-//            numberOfUnits = "1";
-//            unitOfMeasure = word;
-//            break;
-//          case "tablets":
-//            unitOfMeasure = "tablet";
-//            if (checkIfWordNumber(prevWord)) { //checks if its a number spelt out as a word
-//              numberOfUnits = convertWordNumber(prevWord); //converts the word into a number but in string format
-//            } else if (checkIfNumber(prevWord)) {
-//              numberOfUnits = prevWord;
-//            }
-//            break;
-//          case "capsules":
-//            unitOfMeasure = "capsule";
-//            if (checkIfWordNumber(prevWord)) { //checks if its a number spelt out as a word
-//              numberOfUnits = convertWordNumber(prevWord); //converts the word into a number but in string format
-//            } else if (checkIfNumber(prevWord)) {
-//              numberOfUnits = prevWord;
-//            }
-//            break;
-//
-//
-//        }
-//
-//
-//      }
-//    }
-//
-//
-//  }
-//
-//
-//   bool checkIfWordNumber(String prevWord) {
-//    switch (prevWord) {
-//      case "one":
-//      case "two":
-//      case "three":
-//      case "four":
-//      case "five":
-//      case "six":
-//      case "seven":
-//      case "eight":
-//      case "nine":
-//      case "ten":
-//        return true;
-//      default:
-//        return false;
-//    }
-//  }
-//
-//   String convertWordNumber(String prevWord) {
-//    switch (prevWord) {
-//      case "one":
-//        return "1";
-//      case "two":
-//        return "2";
-//      case "three":
-//        return "3";
-//      case "four":
-//        return "4";
-//      case "five":
-//        return "5";
-//      case "six":
-//        return "6";
-//      case "seven":
-//        return "7";
-//      case "eight":
-//        return "8";
-//      case "nine":
-//        return "9";
-//      case "ten":
-//        return "10";
-//      default:
-//        return "0"; //check if this is a valid default
-//    }
-//  }
-//
-//    bool checkIfNumber(String prevWord) {
-//    switch (prevWord) {
-//      case "1":
-//      case "10":
-//      case "9":
-//      case "8":
-//      case "7":
-//      case "6":
-//      case "5":
-//      case "4":
-//      case "3":
-//      case "2":
-//        return true;
-//      default:
-//        return false;
-//    }
-//
-//  }
-//
-//
-//
-//
-//}
-//
-//
+   String text="";
+    for (TextBlock block in visionText.blocks) {
+      for (TextLine line in block.lines) {
+        for (TextElement word in line.elements) {
+          text = text + word.text + ' ';
+        }
+        text = text + '\n';
+      }
+    }
+
+    return text;
+  }
+
+  void categorizeBlocks(VisionText visionText) {
+
+    blocks = visionText.blocks;
+    if (blocks.isEmpty) {
+//    Toast.makeText(this, "No text in image", Toast.LENGTH_SHORT).show();
+      print('No text');
+    }
+    else {
+      int i=0;
+      for (TextBlock block in visionText.blocks) {
+        // System.out.println(blocks.get(i).getText().toString().toLowerCase());
+        List<TextLine> paras = block.lines;
+        for (TextLine para in paras) {
+          List<TextElement> elements = para.elements;
+          //System.out.println(elements.get(2).getText());
+          for (int k=0;k<elements.length;k++) {
+            String word = elements[k].toString().toLowerCase();
+
+            if (((word == "take") || (word == "taken")) &&
+                (directionsOfUseBlock == 100000)) {
+              print("FOUND DIRECTIONS");
+              directionsOfUseBlock = i;
+//              extractDetails(blocks);
+            }
+            String mySubstring = null;
+            if (word.length > 2) {
+              mySubstring = word.substring(word.length - 2, word.length);
+            }
+
+            //System.out.println(mySubstring);
+            if (word.length > 2 && ((mySubstring.compareTo("mg")) == 0 ||
+                (mySubstring.compareTo("ml")) == 0)) {
+              print("you should only see this once");
+              //System.out.println(word);
+              medicineNameBlock = i;
+              if (k - 1 > -1) {
+                //System.out.println(elements.get(k - 1));
+                medicineName2 =  elements[k - 1].toString();
+
+                if (k - 2 > -1) {
+                  // System.out.println("med1");
+                  medicineName1 = elements[k - 2].toString();
+                }
+              }
+            }//else if (word.length() > 1 && (word.substring(word.length() - 1, word.length() - 1).equals("g") || (word.substring(word.length() - 1, word.length() - 1).equals("l")))) {
+            //medicineNameBlock = i;
+          } //System.out.println("Next statement");
+        }
+        i++;
+      }
+    }
+  }
+
+  void extractDetails(List<TextBlock> blocks) {
+    List<TextLine> paras = blocks[directionsOfUseBlock].lines;
+    for (int j = 0; j < paras.length; j++) {
+      List<TextElement> words = paras[j].elements;
+
+      for (int k = 0; k < words.length; k++) {
+        String word = words[k].toString().toLowerCase().trim();
+        //System.out.println(word);
+        String prevWord = null;
+        if (k != 0) {
+          prevWord =words[k-1].toString().toLowerCase().trim();
+        }
+        switch (word) {
+          case "daily":
+          case "weekly":
+          case "monthly":
+            dayFrequency = word;
+            break;
+          case "day":
+            if (prevWord=="a") {
+              dayFrequency = "daily";
+            }
+            break;
+          case "week":
+            if (prevWord=="a") {
+              dayFrequency = "weekly";
+            }
+            break;
+          case "month":
+            if (prevWord=="a") {
+              dayFrequency = "monthly";
+            }
+            break;
+          case "once":
+          case "time":
+            frequency = "1";
+            break;
+          case "twice":
+            frequency = "2";
+            break;
+          case "thrice":
+            frequency = "3";
+            break;
+          case "times":
+            if (checkIfWordNumber(prevWord)) { //checks if its a number spelt out as a word
+              frequency = convertWordNumber(prevWord); //converts the word into a number but in string format
+            } else if (checkIfNumber(prevWord)) {
+              frequency = prevWord;
+            }
+            break;
+          case "tablet":
+          case "capsule":
+            numberOfUnits = "1";
+            unitOfMeasure = word;
+            break;
+          case "tablets":
+            unitOfMeasure = "tablet";
+            if (checkIfWordNumber(prevWord)) { //checks if its a number spelt out as a word
+              numberOfUnits = convertWordNumber(prevWord); //converts the word into a number but in string format
+            } else if (checkIfNumber(prevWord)) {
+              numberOfUnits = prevWord;
+            }
+            break;
+          case "capsules":
+            unitOfMeasure = "capsule";
+            if (checkIfWordNumber(prevWord)) { //checks if its a number spelt out as a word
+              numberOfUnits = convertWordNumber(prevWord); //converts the word into a number but in string format
+            } else if (checkIfNumber(prevWord)) {
+              numberOfUnits = prevWord;
+            }
+            break;
+
+
+        }
+
+
+      }
+    }
+
+
+  }
+
+
+   bool checkIfWordNumber(String prevWord) {
+    switch (prevWord) {
+      case "one":
+      case "two":
+      case "three":
+      case "four":
+      case "five":
+      case "six":
+      case "seven":
+      case "eight":
+      case "nine":
+      case "ten":
+        return true;
+      default:
+        return false;
+    }
+  }
+
+   String convertWordNumber(String prevWord) {
+    switch (prevWord) {
+      case "one":
+        return "1";
+      case "two":
+        return "2";
+      case "three":
+        return "3";
+      case "four":
+        return "4";
+      case "five":
+        return "5";
+      case "six":
+        return "6";
+      case "seven":
+        return "7";
+      case "eight":
+        return "8";
+      case "nine":
+        return "9";
+      case "ten":
+        return "10";
+      default:
+        return "0"; //check if this is a valid default
+    }
+  }
+
+    bool checkIfNumber(String prevWord) {
+    switch (prevWord) {
+      case "1":
+      case "10":
+      case "9":
+      case "8":
+      case "7":
+      case "6":
+      case "5":
+      case "4":
+      case "3":
+      case "2":
+        return true;
+      default:
+        return false;
+    }
+
+  }
+
+
+
+
+}
+
+
--- lib/form.dart
+++ lib/form.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:image_picker/image_picker.dart';
+import 'package:medhelp/api/firebase_ml_api.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'database.dart';
-
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 class Prescription extends StatefulWidget {
   @override
   _PrescriptionState createState() => _PrescriptionState();
@@ -14,7 +21,107 @@ class _PrescriptionState extends State<Prescription> {
   String description='';
   String medicines='';
   String error='';
-  User user = FirebaseAuth.instance.currentUser;
+  bool isSubmitted = false;
+  bool isLoading;
+  User user;
+  bool _loadingPath = false;
+  List<PlatformFile> _paths;
+  String presctext="";
+  FileType _pickingType = FileType.any;
+  String _directoryPath;
+  String fileName;
+  bool isLate = false;
+  String _extension;
+  bool _multiPick = false;
+  String userAssignmentUrl = "";
+  DateTime userSubmissionTime;
+  String userFileName = "";
+  String userGrade = "";
+String url="";
+  Future getFileAndUpload() async {
+//    FilePickerResult result;
+//    try {
+//      _directoryPath = null;
+//     result = await FilePicker.platform.pickFiles();
+//    } on PlatformException catch (e) {
+//      print("Unsupported operation" + e.toString());
+//    } catch (ex) {
+//      print(ex);
+//    }
+//    if (!mounted) return;
+//    List<int> bytes;
+//    if(result != null) {
+//      PlatformFile file = result.files.first;
+//      bytes = file.bytes;
+//      print(file.name);
+//      print(file.bytes);
+//      print(file.size);
+//      print(file.extension);
+//      print(file.path);
+//      Uint8List data = Uint8List.fromList(bytes);
+//      SnackBar snackBar =
+//      SnackBar(content: Text("Uploading Your Prescription....."));
+//      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+//      File filee  =File.fromRawPath(data);
+//      presctext= await FirebaseMLApi.recogniseText(filee);
+//      Upload(fileName, data);
+//    }
+
+    dynamic imgFile = await ImagePicker.platform.pickImage(
+        source: ImageSource.gallery,
+
+    );
+    List<File> images = [];
+    setState(() {
+      images.add(imgFile);
+    });
+    Upload(userSubmissionTime.toString(), images[0]);
+//    presctext=await FirebaseMLApi.recogniseText(file);
+    setState(() async{
+      _loadingPath = false;
+
+//      _paths != null ? _paths.map((e) => fileName = e.name).toString() : '...';
+
+//      _paths != null ? _paths.map((e) => {bytes = e.bytes}).toString() : '...'
+
+
+    });
+  }
+
+  Upload(fileName, dynamic data) async {
+    setState(() => _loadingPath = true);
+    firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance.ref(
+        'usersdata/${user.uid}/${userSubmissionTime}/${fileName}');
+    _extension = fileName.toString().split('.').last;
+    firebase_storage.SettableMetadata metadata =
+    firebase_storage.SettableMetadata(
+        contentType: '$_pickingType/$_extension');
+
+    try {
+      await ref.putFile(data, metadata);
+      String downloadURL = await ref.getDownloadURL();
+      print(downloadURL);
+//      storeToFirestore(downloadURL, fileName);
+      setState(() {
+        url=downloadURL;
+        _loadingPath = false;
+      });
+    } catch (e) {
+      SnackBar snackBar = SnackBar(content: Text(e.toString()));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      print(e);
+    }
+    setState(() {
+      _loadingPath=false;
+    });
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    user=FirebaseAuth.instance.currentUser;
+    userSubmissionTime=DateTime.now();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,6 +175,24 @@ class _PrescriptionState extends State<Prescription> {
                 ),
                 SizedBox(height: 20.0),
 
+             _loadingPath?Center(child: CircularProgressIndicator(),) :   ElevatedButton(
+//                    color: Colors.pink[400],
+                    child: Text("Upload Prescription"),
+                    onPressed: (){
+                      print(drname);
+                      print(description);
+                      print(medicines);
+//                      if(key.currentState.validate()){
+//                      DatabaseService(uid: user.uid).adddata(drname, description, medicines);
+//                      Navigator.pop(context);
+                    getFileAndUpload();
+//                      }
+                    }
+
+                ),
+
+                SizedBox(height: 20.0),
+
                 ElevatedButton(
 //                    color: Colors.pink[400],
                     child: Text("Add"),
@@ -76,7 +201,7 @@ class _PrescriptionState extends State<Prescription> {
                       print(description);
                       print(medicines);
 //                      if(key.currentState.validate()){
-                         DatabaseService(uid: user.uid).adddata(drname, description, medicines);
+                         DatabaseService(uid: user.uid).adddata(doctor: drname,description: description,medicines: medicines,url: url,prescText: presctext);
                         Navigator.pop(context);
 //                      }
                     }
